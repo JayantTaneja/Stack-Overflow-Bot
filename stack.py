2a6717159465b772fefcecc4fce436e38c18507f
Jayant Taneja <jayanttaneja1@gmail.com>
2021-03-08 12:22:45 +0530

    update stack.py

--- stack.py
+++ stack.py
@@ -1,3 +1,4 @@
+from docopt import docopt
 import webbrowser as wb
 from tkinter import Tk
 
@@ -9,3 +10,30 @@ def bot():
     chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
     wb.get(chrome_path).open(url)
+
+
+usage = '''
+
+StackOverFlow Bot
+
+Usage:
+  stack run
+  stack help
+
+'''
+args = docopt(usage)
+
+if args['run']:
+    
+    bot()
+
+if args['help']:
+
+    print("-----------------------------------------------------------------------\n\t\t\t\tWelcome !!")
+    print("-----------------------------------------------------------------------\nThis bot is aimed to make your life easier by automatically searching")
+    print("clipboard text on stackoverflow")
+
+    print("\nHere is how to use this bot:\n")
+
+    print("1) Simply copy an error message")
+    print("2) Type 'stack run' in your terminal")
